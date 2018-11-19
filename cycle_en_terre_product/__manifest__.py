64f9b2339a7f4c7eda0ca39b3293501eb1b28a32
robinkeunen <robin@keunen.net>
2018-11-19 09:57:21 +0100

    [ADD] cet_template: add model fields

---
+++ cycle_en_terre_product/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    'name': "Cycle en Terre - Products",
+
+    'summary': """
+        Customization of product_template for Cycle en Terre""",
+
+    'author': "Coop IT Easy SCRL",
+    'website': "https://www.coopiteasy.be",
+    'license': 'AGPL-3',
+
+    'category': 'Uncategorized',
+    'version': '11.0.0.0.1',
+
+    'depends': [
+        'product'
+    ],
+
+    'data': [
+        'views/product_template.xml',
+    ],
+    'demo': [
+        'demo/demo.xml',
+    ],
+    'installable': True,
+}
---
+++ cycle_en_terre_product/models/product_template.py
@@ -0,0 +1,46 @@
+from odoo import models, fields
+
+
+class SeedlingMonth(models.Model):
+    _name = 'seedling.month'
+    _order = 'sequence'
+
+    sequence = fields.Integer(
+        string='Sequence',
+        required=True,
+        readonly=True,
+    )
+    name = fields.Char(
+        string="Name",
+        required=True,
+        readonly=True,
+    )
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    species = fields.Char(
+        string='Species',
+    )
+    latin_name = fields.Char(
+        string='Latin Name',
+    )
+    emergence = fields.Integer(
+        string='Emergence (days)',
+    )
+    density = fields.Float(
+        string='Density (g/10 m²)',
+    )
+    germination = fields.Float(
+        string='Germination (years)'
+    )
+    species_information = fields.Text(
+        string='Species Information',
+    )
+    culture_information = fields.Text(
+        string='Culture Information',
+    )
+    recipe = fields.Html(
+        string='Recipe',
+    )
