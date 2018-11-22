74fd32ceb2ec6efb46ae234bccd2fea933c9c65e
nicolasj77 <nicolasjamoulle@yahoo.fr>
2018-11-22 16:14:56 +0100

    Init module and an link stylesheet less

---
+++ cycle_en_terre_website_design/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- encoding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Cycle en terre Website design',
+    'category': 'Website',
+    'summary': 'Design modification to look like their own WordPress Website http://www.cycle-en-terre.be',
+    'website': 'http://www.cycle-en-terre.be',
+    'author': 'Nicolas Jamoulle <nicolas@coopiteasy.be>',
+    'depends': ['website'],
+    'installable': True,
+    'data': [
+        'views/website_templates.xml',
+    ],
+
+}
