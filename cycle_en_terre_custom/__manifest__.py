3bd4d8a125a2a02ad5405f99f0728549f73c60eb
robinkeunen <robin@keunen.net>
2018-12-04 18:01:42 +0100

    [IMP] cet_custom: sort pickings by product location

---
+++ cycle_en_terre_custom/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': "Cycle en Terre - Custom Modifications",
+
+    'summary': """
+        Customizations for Cycle en Terre""",
+
+    'author': "Coop IT Easy SCRL",
+    'website': "https://www.coopiteasy.be",
+    'license': 'AGPL-3',
+
+    'category': 'Uncategorized',
+    'version': '11.0.0.0.1',
+
+    'depends': [
+        'stock',
+    ],
+
+    'data': [
+        'report/report_stockpicking_operations.xml',
+    ],
+    'installable': True,
+}
