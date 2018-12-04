bfbc119e45fb7f8478baf5444f8cf861d0f4b529
Rémy Taymans <remytaymans@gmail.com>
2018-12-04 21:54:25 +0100

    [ADD] w_s_category_breadcrumb: Nice category Breadcrumb

---
+++ website_sale_category_breadcrumb/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2018 Coop IT Easy SCRLfs <https://coopiteasy.be>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Website Sale Category Breadcrumb",
+    "summary": "Nice Breadcrumb for categories in the e-Commerce",
+    "version": "11.0.1.0.0",
+    "category": "e-commerce",
+    "website": "https://github.com/coopiteasy/cycle-en-terre/",
+    "author": "Coop IT Easy SCRLfs",
+    "license": "AGPL-3",
+    "application": False,
+    'installable': True,
+    "depends": [
+        "website_sale",
+    ],
+    "data": [
+        "views/templates.xml",
+    ],
+}
