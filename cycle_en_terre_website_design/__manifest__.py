cdd6fd2f8717a99e0da13cf40f2fe42508a7b645
nicolasj77 <nicolasjamoulle@yahoo.fr>
2018-12-07 18:46:55 +0100

    Coorections after remytms's reviews

--- cycle_en_terre_website_design/__manifest__.py
+++ cycle_en_terre_website_design/__manifest__.py
@@ -1,12 +1,12 @@
-# -*- encoding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
+# Copyright 2018 Coop IT Easy SCRLfs <https://coopiteasy.be>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Cycle en terre Website design',
     'category': 'Website',
     'summary': 'Design modification to look like their own WordPress Website '
                'http://www.cycle-en-terre.be',
-    'website': 'http://www.cycle-en-terre.be',
+    'website': 'https://github.com/coopiteasy/cycle-en-terre',
     'author': 'Nicolas Jamoulle <nicolas@coopiteasy.be>',
     'depends': ['website'],
     'installable': True,
