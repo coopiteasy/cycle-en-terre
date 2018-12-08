55a2a6ea3e15999d62b5ea974220a61422dfa32b
robinkeunen <robin@keunen.net>
2018-12-08 13:15:24 +0100

    [IMP] product_seeds: setting default variant to true resets others to false

--- product_seeds/models/product_product.py
+++ product_seeds/models/product_product.py
@@ -1,7 +1,7 @@
 # © 2016 Robin Keunen, Coop IT Easy SCRL fs
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class ProductProduct(models.Model):
@@ -13,12 +13,31 @@ class ProductProduct(models.Model):
     variant_sale_ok = fields.Boolean(
         string='Can be Sold (Variant)',
     )
-    # todo set other variants default to false (on change)
     default_variant = fields.Boolean(
         string='Default Variant',
+        compute='_compute_default_variant',
+        inverse='_inverse_default_variant',
+        store=True,
     )
     # todo migrate to many2many
     label = fields.Char(
         string="Label",
     )
     # todo weight unit of measure
+
+    def _compute_default_variant(self):
+        return self.default_variant
+
+    def _inverse_default_variant(self):
+        self.ensure_one()
+        if self.default_variant:
+            other_variants = (
+                self
+                .product_tmpl_id
+                .product_variant_ids
+                .filtered(lambda p: p.id != self.id)
+            )
+            for product in other_variants:
+                product.default_variant = False
+
+        return
