67db05b78b48702a2ff4801279f4e343f171429a
Rémy Taymans <remytaymans@gmail.com>
2019-04-04 11:22:18 +0200

    [FIX] cet_w_sale: Linting

--- cet_website_sale/static/src/js/stock.js
+++ cet_website_sale/static/src/js/stock.js
@@ -1,103 +1,107 @@
-odoo.define('cet_website_sale', function(require) {
-'use strict';
+odoo.define('cet_website_sale', function (require) {
+    'use strict';
 
-require('web.dom_ready');
-var base = require('web_editor.base');
-var ajax = require('web.ajax');
-var core = require('web.core');
+    require('web.dom_ready');
+    var ajax = require('web.ajax');
 
 
-if(!$('.oe_website_sale').length) {
-  return $.Deferred().reject("DOM doesn't contain '.oe_website_sale'");
-}
+    if (!$('.oe_website_sale').length) {
+        return $.Deferred().reject("DOM doesn't contain '.oe_website_sale'");
+    }
 
-function check_stock(event) {
-  /*
-   * This function get information about stock for a product variant and
-   * it disable action button when needed. It also show messages about
-   * the stock when appropriated.
-   */
-  // The variable 'this' should contain an input with value equal to the
-  // product variant id.
-  var input = this;
+    function check_stock () {
 
-  ajax.jsonRpc("/shop/product/stock_info",
-    'call',
-    {'id': parseInt($(input).attr("value"))})
-    .then(function(stock_info){
-      // Select element on the page
-      var js_product = $(input).closest('.js_product');
-      var qty_input = $(js_product).find('input[name="add_qty"]');
-      var qty = qty_input.val();
+        /**
+         * This function gets information about stock for a product
+         * variant and it disables action button when needed. It also
+         * shows messages about the stock when appropriated.
+         */
 
-      // Check only if the inventory_availability is 'always' or
-      // 'threshold'
-      if (stock_info['inventory_availability'] === 'always'
-        || stock_info['inventory_availability'] === 'threshold') {
+        // The variable 'this' must contain an input with value equal
+        // to the product variant id.
+        var input = this;
 
-        // If there is a threshold, remove it from the available quantity
-        if (stock_info['inventory_availability'] === 'threshold') {
-          stock_info['virtual_available'] -= stock_info['available_threshold'];
-        }
+        ajax.jsonRpc("/shop/product/stock_info",
+            'call',
+            {'id': parseInt($(input).attr("value"), 10)})
+            .then(function (stock_info) {
+                // Select element on the page
+                var js_product = $(input).closest('.js_product');
+                var qty_input = $(js_product).find('input[name="add_qty"]');
+                var qty = qty_input.val();
 
-        // Remove quantity in a cart from the available quantity
-        stock_info['virtual_available'] -= parseInt(stock_info['cart_qty']);
+                // Check only if the inventory_availability is 'always' or
+                // 'threshold'
+                if (stock_info.inventory_availability === 'always' ||
+                    stock_info.inventory_availability === 'threshold') {
 
-        // Set quantity to 0 in case of a negative value after
-        // computations
-        if (stock_info['virtual_available'] < 0) {
-          stock_info['virtual_available'] = 0;
-        }
+                    // If there is a threshold, remove it from the
+                    // available quantity
+                    if (stock_info.inventory_availability === 'threshold') {
+                        stock_info.virtual_available -=
+                            stock_info.available_threshold;
+                    }
 
-        // Prevent qty to grow above the available quantity
-        if (qty > stock_info['virtual_available']) {
-          qty = stock_info['virtual_available'] || 1;
-          qty_input.val(qty);
-        }
+                    // Remove quantity in a cart from the available quantity
+                    stock_info.virtual_available -=
+                        parseInt(stock_info.cart_qty, 10);
 
-        // Disable button "Add to cart"
-        var disable_condition = (
-          qty > stock_info['virtual_available']
-          || stock_info['virtual_available'] < 1
-          || qty < 1
-        );
-        $(js_product).find('#add_to_cart').toggleClass(
-          'disabled',
-          disable_condition
-        );
+                    // Set quantity to 0 in case of a negative value after
+                    // computations
+                    if (stock_info.virtual_available < 0) {
+                        stock_info.virtual_available = 0;
+                    }
 
-        // Disable 'add quantity' button when reaching maximum
-        // available quantity.
-        $(js_product).find('.js_add').toggleClass(
-          'disabled btn',
-          stock_info['virtual_available'] - qty < 1
-        );
-        $(js_product).find('.js_remove').toggleClass(
-          'disabled btn',
-          qty <= 1
-        );
+                    // Prevent qty to grow above the available quantity
+                    if (qty > stock_info.virtual_available) {
+                        qty = stock_info.virtual_available || 1;
+                        qty_input.val(qty);
+                    }
 
-        // Display message
-        $(js_product).find('.oe_stock_messages').toggleClass(
-          'hidden',
-          stock_info['virtual_available'] - qty >= 1
-        );
-      }
+                    // Disable button "Add to cart"
+                    var disable_condition =
+                        qty > stock_info.virtual_available ||
+                        stock_info.virtual_available < 1 ||
+                        qty < 1
+                    ;
+                    $(js_product).find('#add_to_cart').toggleClass(
+                        'disabled',
+                        disable_condition
+                    );
 
+                    // Disable 'add quantity' button when reaching maximum
+                    // available quantity.
+                    $(js_product).find('.js_add').toggleClass(
+                        'disabled btn',
+                        stock_info.virtual_available - qty < 1
+                    );
+                    $(js_product).find('.js_remove').toggleClass(
+                        'disabled btn',
+                        qty <= 1
+                    );
+
+                    // Display message
+                    $(js_product).find('.oe_stock_messages').toggleClass(
+                        'hidden',
+                        stock_info.virtual_available - qty >= 1
+                    );
+                }
+
+            });
+    }
+
+    $('.oe_website_sale').each(function () {
+        var oe_website_sale = this;
+
+        // At page load, run check_stock() on each input containing a product_id
+        $(oe_website_sale).find('input[name="product_id"]').each(check_stock);
+
+        // Run check_stock() when changing status of an input
+        $(oe_website_sale).find('input[name="product_id"]')
+            .on('change', check_stock);
+
+        // Run check_stock() when input quantity status changed
+        $(oe_website_sale).find('input[name=qty]').on('change', check_stock);
     });
-}
-
-$('.oe_website_sale').each(function() {
-  var oe_website_sale = this;
-
-  // At page load, run check_stock() on each input containing a product_id
-  $(oe_website_sale).find('input[name="product_id"]').each(check_stock);
-
-  // Run check_stock() when changing status of an input
-  $(oe_website_sale).find('input[name="product_id"]').on('change', check_stock);
-
-  // Run check_stock() when input quantity status changed
-  $(oe_website_sale).find('input[name=qty]').on('change', check_stock);
-});
 
 });
