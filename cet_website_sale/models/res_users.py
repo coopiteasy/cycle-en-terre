99e971ae6d883be15290ed29b4596d66f7f4bafc
Vincent Van Rossem <vvrossem@gmail.com>
2021-02-03 14:10:22 +0100

    [ADD] cet_website_sale: tax incl./excl. toggle

---
+++ cet_website_sale/models/res_users.py
@@ -0,0 +1,34 @@
+from odoo import api, models
+from odoo.http import request
+from odoo.tools import ormcache
+
+
+class ResUsers(models.Model):
+    _inherit = "res.users"
+
+    # Taken from oca/e-commerce/website_sale_tax_toggle
+    # Start of addition
+    @api.model
+    def has_group(self, group_ext_id):
+        tax_included = 'sale.group_show_price_total'
+        tax_excluded = 'sale.group_show_price_subtotal'
+        if (self.env.context.get('website_id') and
+                group_ext_id in [tax_included, tax_excluded] and
+                not self.env.context.get('skip_tax_toggle_check')):
+            taxed = request.session.get(
+                'tax_toggle_taxed',
+                self.env.user.with_context(
+                    skip_tax_toggle_check=True
+                ).has_group(tax_included)
+            )
+            return group_ext_id != (tax_excluded if taxed else tax_included)
+        return super().has_group(group_ext_id)
+
+    # HACK: To clear cache called from res.users write method
+    @api.model
+    @ormcache('self._uid', 'group_ext_id')
+    def _has_group(self, group_ext_id):
+        return super()._has_group(group_ext_id)
+
+    has_group.clear_cache = _has_group.clear_cache
+    # End of addition
