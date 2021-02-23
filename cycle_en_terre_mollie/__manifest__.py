adf3f2ec047808823cf61544dc009ae5288aa13b
Vincent Van Rossem <vvrossem@gmail.com>
2021-02-23 18:08:55 +0100

    [ADD] cycle_en_terre_mollie

---
+++ cycle_en_terre_mollie/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2018-2021 Coop IT Easy SCRLfs <https://coopiteasy.be>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Cycle en Terre - Mollie",
+    "summary": """
+        Mollie customizations for Cycle en Terre""",
+    "author": "Coop IT Easy SCRL",
+    "website": "https://www.coopiteasy.be",
+    "license": "AGPL-3",
+    "category": "eCommerce",
+    "version": "11.0.0.0.1",
+    "depends": ["payment_mollie_official", "l10n_be_sale_order_bba"],
+    "data": ["views/payment_mollie_templates.xml",],
+    "installable": True,
+}
